182e984599a075a38a5c9c64e1018c5e2ed63564
SuungYul <pmtf00@gmail.com>
2023-05-07 22:11:51 +0900

    시원 5분줄게

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,10 +1,16 @@
-import React from 'react';
-import Messenger from '../Messenger';
+import React, { useState } from "react";
+import Messenger from "../Messenger";
+import RegisterPage from "../RegisterPage";
+import LoginPage from "../LoginPage";
 
 export default function App() {
-    return (
-      <div className="App">
-        <Messenger />
-      </div>
-    );
+  // const [isLogin, setLogin] = useState(false);
+  return (
+    <div className="App">
+      {/* {isLogin ? <Messenger /> : <LoginPage />} */}
+      {/* <Messenger /> */}
+      {/* <RegisterPage /> */}
+      <LoginPage />
+    </div>
+  );
 }
