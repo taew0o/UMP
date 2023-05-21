7918ebf485b2e4b89f429c16cdd8e2d767f018fb
SuungYul <pmtf00@gmail.com>
2023-05-21 22:32:18 +0900

    Review

--- frontend/UMP-5min/src/components/MessageList/index.js
+++ frontend/UMP-5min/src/components/MessageList/index.js
@@ -7,6 +7,8 @@ import moment from "moment";
 
 import "./MessageList.css";
 import { useLocation, useParams } from "react-router-dom";
+import ReactModal from "react-modal";
+import Review from "../Review/Review";
 
 const MY_USER_ID = "apple";
 
@@ -18,10 +20,22 @@ export default function MessageList(props) {
 
   const [result, setResult] = useState([]);
   const [text, setText] = useState();
-  // useEffect(() => {
-  //   renderMessages();
-  //   // console.log(messages);
-  // }, []);
+
+  const customStyles = {
+    content: {
+      top: "35%",
+      left: "50%",
+      right: "auto",
+      bottom: "auto",
+      marginRight: "-50%",
+      height: "50%",
+      width: "50%",
+      transform: "translate(-40%, -10%)",
+    },
+  };
+
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+
   useEffect(() => {
     console.log(text);
     makeMsg();
@@ -39,11 +53,11 @@ export default function MessageList(props) {
       setMessages([...messages, ...text]);
     }
   };
-  
+
   useEffect(() => {
     renderMessages();
   }, [messages]);
-  
+
   const getMessages = () => {
     var tempMessages = [
       {
@@ -186,12 +200,20 @@ export default function MessageList(props) {
       <Toolbar
         title={"test"}
         rightItems={[
+          <div
+            onClick={() => {
+              if (window.confirm(`이 채팅방을 나가시겠습니까?`)) {
+                setModalIsOpen(true);
+              }
+              console.log("클릭");
+            }}
+          >
+            <ToolbarButton key="exit" icon="ion-ios-exit" />
+          </div>,
           <ToolbarButton
             key="info"
             icon="ion-ios-information-circle-outline"
           />,
-          <ToolbarButton key="video" icon="ion-ios-videocam" />,
-          <ToolbarButton key="phone" icon="ion-ios-call" />,
         ]}
       />
 
@@ -201,15 +223,15 @@ export default function MessageList(props) {
         messages={messages}
         getText={getText}
         setMessages={setMessages}
-        rightItems={[
-          <ToolbarButton key="photo" icon="ion-ios-camera" />,
-          <ToolbarButton key="image" icon="ion-ios-image" />,
-          <ToolbarButton key="audio" icon="ion-ios-mic" />,
-          <ToolbarButton key="money" icon="ion-ios-card" />,
-          <ToolbarButton key="games" icon="ion-logo-game-controller-b" />,
-          <ToolbarButton key="emoji" icon="ion-ios-happy" />,
-        ]}
       />
+
+      <ReactModal
+        isOpen={modalIsOpen}
+        onRequestClose={() => setModalIsOpen(false)}
+        style={customStyles}
+      >
+        <Review />
+      </ReactModal>
     </div>
   );
 }
---
+++ frontend/UMP-5min/src/components/Review/Review.js
@@ -0,0 +1,93 @@
+import { Button } from "antd";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import "./Review.css"; // Review 컴포넌트를 위한 CSS 파일 import
+
+const Review = () => {
+  const navigate = useNavigate();
+  const toChat = () => {
+    navigate("/");
+  };
+  const user = ["박태우", "이새롬", "박지후"];
+
+  const initialUserReviews = user.map((userName) => ({
+    name: userName,
+    attend: 0,
+    late: 0,
+    absence: 0,
+  }));
+
+  const [userReviews, setUserReviews] = useState(initialUserReviews);
+
+  const handleReviewClick = (index, type) => {
+    setUserReviews((prevState) => {
+      const updatedUserReviews = [...prevState];
+      const selectedReview = updatedUserReviews[index];
+
+      if (selectedReview[type] === 0) {
+        selectedReview[type]++;
+
+        Object.keys(selectedReview).forEach((key) => {
+          if (key !== type && key !== "name") {
+            selectedReview[key] = 0;
+          }
+        });
+      }
+
+      return updatedUserReviews;
+    });
+  };
+
+  const userReview = userReviews.map((review, index) => (
+    <div key={index}>
+      <span>{review.name + "에 대한 평가 "}</span>
+      <Button
+        onClick={() => handleReviewClick(index, "attend")}
+        type={review.attend > 0 ? "primary" : ""}
+      >
+        참석
+      </Button>
+      <Button
+        onClick={() => handleReviewClick(index, "late")}
+        type={review.late > 0 ? "primary" : ""}
+      >
+        지각
+      </Button>
+      <Button
+        onClick={() => handleReviewClick(index, "absence")}
+        type={review.absence > 0 ? "primary" : ""}
+      >
+        불참
+      </Button>
+    </div>
+  ));
+
+  const totalAttend = userReviews.reduce(
+    (total, review) => total + review.attend,
+    0
+  );
+  const totalLate = userReviews.reduce(
+    (total, review) => total + review.late,
+    0
+  );
+  const totalAbsence = userReviews.reduce(
+    (total, review) => total + review.absence,
+    0
+  );
+
+  return (
+    <div className="review-container">
+      <div className="review-heading">평가</div>
+      <div className="user-review">{userReview}</div>
+      <span className="total-stats">
+        총 인원 {user.length}명 (참석 {totalAttend}명 / 지각 {totalLate}명 /
+        불참 {totalAbsence}명)
+      </span>
+      <Button className="to-chat-button" onClick={toChat}>
+        제출
+      </Button>
+    </div>
+  );
+};
+
+export default Review;
