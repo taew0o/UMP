6cebf8c18ef5b1448d56da2e2c3c50c319aab929
coolman0123 <socool4m@naver.com>
2023-05-21 19:53:13 +0900

    toolbaV2
    ToolbarV2

--- frontend/UMP-5min/src/components/Toolbar/index.js
+++ frontend/UMP-5min/src/components/Toolbar/index.js
@@ -1,13 +1,43 @@
-import React from 'react';
-import './Toolbar.css';
+import React from "react";
+import { Button, Popover, Input } from "antd";
+import "./Toolbar.css";
 
 export default function Toolbar(props) {
-    const { title, leftItems, rightItems } = props;
-    return (
-      <div className="toolbar">
-        <div className="left-items">{ leftItems }</div>
-        <h1 className="toolbar-title">{ title }</h1>
-        <div className="right-items">{ rightItems }</div>
-      </div>
-    );
+  const { title, leftItems, rightItems, popoverContent } = props;
+
+  const [visible, setVisible] = React.useState(false);
+
+  const handleAddButtonClick = () => {
+    // This line is no longer necessary
+    // setVisible(!visible);
+  };
+
+  const handleVisibleChange = (visible) => {
+    setVisible(visible);
+  };
+
+  const rightItemsWithPopover = rightItems.map((item, index) => {
+    if (item.key === "add") {
+      return (
+        <Popover
+          key={index}
+          content={popoverContent}
+          trigger="click"
+          onVisibleChange={handleVisibleChange}
+        >
+          <div onClick={handleAddButtonClick}>{item}</div>
+        </Popover>
+      );
+    } else {
+      return item;
+    }
+  });
+
+  return (
+    <div className="toolbar">
+      <div className="left-items">{leftItems}</div>
+      <h1 className="toolbar-title">{title}</h1>
+      <div className="right-items">{rightItemsWithPopover}</div>
+    </div>
+  );
 }
