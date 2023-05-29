09167592ee084cc0b529f0a10e4a9ff6930d4328
SuungYul <pmtf00@gmail.com>
2023-05-29 23:05:04 +0900

    merge

--- src/main/java/ppkjch/ump/controller/SessionInterceptor.java
+++ src/main/java/ppkjch/ump/controller/SessionInterceptor.java
@@ -1,6 +1,5 @@
 package ppkjch.ump.controller;
 
-import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
@@ -14,25 +13,9 @@ public class SessionInterceptor implements HandlerInterceptor {
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         // 세션 정보 검증 로직을 구현
 
-        Cookie[] cookies = request.getCookies();
-        String sessionId = null;
-
-        if (cookies != null) {
-            for (Cookie cookie : cookies) {
-                if ("sessionId".equals(cookie.getName())) {
-                    sessionId = cookie.getValue();
-                    break;
-                }
-            }
-        }
-
-        if (sessionId == null) {
-            throw new Exception("세션 정보가 없음");
-        }
-
         HttpSession session = request.getSession(false);
+        System.out.println("ssadas"+ request.getCookies().toString());
         System.out.println(session.isNew());
-
         // 세션 정보가 없으면 로그인 페이지로 리다이렉트
         if (session == null || session.getAttribute("JSESSIONID") == null) {
             throw new Exception("세션 정보가 없음");
@@ -42,4 +25,5 @@ public class SessionInterceptor implements HandlerInterceptor {
 
         return true;
     }
+
 }
