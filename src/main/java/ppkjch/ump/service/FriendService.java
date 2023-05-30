024a249f9641a0b9ca4c28f1bcf39e0b4b41307a
ta-mj <kiatae0722@naver.com>
2023-05-30 21:27:48 +0900

    removeFriend 로직 추가

--- src/main/java/ppkjch/ump/service/FriendService.java
+++ src/main/java/ppkjch/ump/service/FriendService.java
@@ -1,5 +1,6 @@
 package ppkjch.ump.service;
 
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import ppkjch.ump.entity.Friend;
@@ -10,16 +11,11 @@ import ppkjch.ump.exception.FriendNotExistException;
 import ppkjch.ump.exception.FriendRequestExistException;
 import ppkjch.ump.repository.JpaFriendRepository;
 import ppkjch.ump.repository.JpaFriendRequestRepository;
-import ppkjch.ump.repository.JpaUserRepository;
-
-import java.util.ArrayList;
 import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class FriendService {
-
-    private final JpaUserRepository jpaUserRepository;
 
     private final JpaFriendRepository jpaFriendRepository;
 
@@ -30,6 +26,7 @@ public class FriendService {
         return jpaFriendRepository.findFriendList(user);
     }
 
+    @Transactional
     public void request(User sender, User receiver){
         if(jpaFriendRequestRepository.findSender(receiver).contains(sender)){
             throw new FriendRequestExistException("이미 해당 유저에게 친구 요청을 하였습니다.");
@@ -46,19 +43,26 @@ public class FriendService {
     public List<User> findFriendRequestList(User receiver){
         return jpaFriendRequestRepository.findSender(receiver);
     }
+
+    @Transactional
     public void addFriend(User u1, User u2){
         Friend friend = new Friend();
         friend.setUser1(u1);
         friend.setUser2(u2);
         jpaFriendRepository.save(friend);
     }
+    @Transactional
+    public void deleteRequest(User sender, User receiver){
+        jpaFriendRequestRepository.deleteRequest(sender,receiver);
+    }
 
+    @Transactional
     public void removeFriend(User u1, User u2){
         if(jpaFriendRepository.findFriend(u1,u2) == null){
             throw new FriendNotExistException("삭제하려는 친구가 존재하지 않습니다");
         }
         else{
-
+            jpaFriendRepository.delete(u1,u2);
         }
     }
 
