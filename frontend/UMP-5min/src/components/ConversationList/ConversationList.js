76e4838441385902e2b4f65bab196df62bde5689
SuungYul <pmtf00@gmail.com>
2023-06-01 20:16:44 +0900

    채팅방 리스트

--- frontend/UMP-5min/src/components/ConversationList/ConversationList.js
+++ frontend/UMP-5min/src/components/ConversationList/ConversationList.js
@@ -18,49 +18,31 @@ export default function ConversationList(props) {
   }, []);
 
   const getConversations = () => {
-    // axios.get("https://randomuser.me/api/?results=20").then((response) => {
-    //   let newConversations = response.data.results.map((result) => {
-    //     return {
-    //       photo: `${result.name.last}`,
-    //       name: `${result.name.first} ${result.name.last}`,
-    //       text: "기존 채팅 기록",
-    //     };
-    //   });
-    //   setConversations([...conversations, ...newConversations]);
-    // });
     axios({
-      method: "post",
-      url: "/chattingroom",
+      method: "get",
+      url: "/chattingrooms",
       headers: {
         "Content-Type": `application/json`,
-      },
-      data: {
-        userIds: friends,
-        roomName: inputValue,
       },
       withCredentials: true,
     })
       .then((response) => {
         console.log("----------------", response);
-        const newChatRoom = {
-          name: inputValue,
-        };
-        setConversations([...conversations, newChatRoom]);
-        setInputValue("");
+        const newConversations = response.data.reduce((acc, value) => {
+          if (value.chattingRoomName !== "") {
+            const newChatRoom = {
+              name: value.chattingRoomName,
+              member: value.numPerson,
+              createTime: value.createTime,
+              id: value.id,
+            };
+            acc.push(newChatRoom);
+          }
+          return acc;
+        }, []);
+        setConversations(newConversations);
       })
       .catch((error) => {
-        if (error.response.status === 400) {
-          if (
-            error.response.data === "해당 유저가 존재하지 않습니다." ||
-            error.response.data === "이미 해당 유저에게 친구 요청을 하였습니다."
-          ) {
-            alert(error.response.data);
-          }
-
-          console.log(error.response.data.message);
-        } else {
-          console.log("기타 에러 발생");
-        }
         console.log(error);
         alert(`에러 발생 관리자 문의하세요!`);
       });
@@ -85,11 +67,8 @@ export default function ConversationList(props) {
     })
       .then((response) => {
         console.log("----------------", response);
-        const newChatRoom = {
-          name: inputValue,
-        };
-        setConversations([...conversations, newChatRoom]);
         setInputValue("");
+        getConversations();
       })
       .catch((error) => {
         if (error.response.status === 400) {
