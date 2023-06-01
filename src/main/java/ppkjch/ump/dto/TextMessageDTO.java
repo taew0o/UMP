8c4262c8635e17ebd7681a364732d8b4bd8e3cf7
wuseong <jws001228@naver.com>
2023-06-01 23:26:18 +0900

    1. UmpWebSocketHandler handleTextMessage 구현중

--- src/main/java/ppkjch/ump/dto/TextMessageDTO.java
+++ src/main/java/ppkjch/ump/dto/TextMessageDTO.java
@@ -8,7 +8,8 @@ import java.time.LocalDateTime;
 @Getter
 @Setter
 public class TextMessageDTO {
-    private Long roomId;
+    private String senderId;
+    private String roomId;
     private String textMsg;
     private LocalDateTime sendTime;
 }
