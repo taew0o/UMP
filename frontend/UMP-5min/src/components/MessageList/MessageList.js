980df08ddf8cc33866487f64e6ba004e48ce4627
coolman0123 <socool4m@naver.com>
2023-06-03 00:32:56 +0900

    chatname
    chatname

--- frontend/UMP-5min/src/components/MessageList/MessageList.js
+++ frontend/UMP-5min/src/components/MessageList/MessageList.js
@@ -59,7 +59,6 @@ export default function MessageList({ props }) {
           timestamp: data.sendTime,
         };
         setMessages((prevMessages) => [...prevMessages, tempMsg]);
-        // renderMessages();
       };
     }
 
@@ -88,7 +87,6 @@ export default function MessageList({ props }) {
       setSendMsg(true);
     }
     console.log("message!!!!!!!", messages);
-    // renderMessages();
   }, [text]);
 
   useEffect(() => {
@@ -102,7 +100,6 @@ export default function MessageList({ props }) {
   const makeMsg = () => {
     if (text) {
       setMessages([...messages, text]);
-      // send();
     }
   };
 
@@ -137,7 +134,7 @@ export default function MessageList({ props }) {
     let i = 0;
     let messageCount = messages.length;
     let tempMessages = [];
-
+  
     while (i < messageCount) {
       let previous = messages[i - 1];
       let current = messages[i];
@@ -149,33 +146,35 @@ export default function MessageList({ props }) {
       let startsSequence = true;
       let endsSequence = true;
       let showTimestamp = true;
-
+  
       if (previous) {
         let previousMoment = moment(previous.timestamp);
         let previousDuration = moment.duration(
           currentMoment.diff(previousMoment)
         );
         prevBySameAuthor = previous.author === current.author;
-
+  
         if (prevBySameAuthor && previousDuration.as("hours") < 1) {
           startsSequence = false;
         }
-
+  
         if (previousDuration.as("hours") < 1) {
           showTimestamp = false;
         }
       }
-
+  
       if (next) {
         let nextMoment = moment(next.timestamp);
         let nextDuration = moment.duration(nextMoment.diff(currentMoment));
         nextBySameAuthor = next.author === current.author;
-
+  
         if (nextBySameAuthor && nextDuration.as("hours") < 1) {
           endsSequence = false;
         }
       }
-
+  
+      const senderId = isMine ? MY_USER_ID : current.author;
+  
       tempMessages.push(
         <Message
           key={i}
@@ -184,14 +183,16 @@ export default function MessageList({ props }) {
           endsSequence={endsSequence}
           showTimestamp={showTimestamp}
           data={current}
+          senderName={senderId} // Pass senderId as senderName prop
         />
       );
-
+  
       // Proceed to the next message.
       i += 1;
     }
     setResult(tempMessages);
   };
+  
 
   return (
     <div className="message-list">
@@ -211,6 +212,7 @@ export default function MessageList({ props }) {
       <div className="message-list-container">{result}</div>
 
       <Compose getText={getText} MY_USER_ID={MY_USER_ID} />
+
 
       <ReactModal
         isOpen={modalIsOpen}
